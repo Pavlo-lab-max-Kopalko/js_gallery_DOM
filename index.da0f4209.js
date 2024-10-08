383e87347e09cc3f51a073188658637a9a746a64
Pavlo <oksanakopalko@gmail.com>
2024-10-08 21:18:00 +0300

    make build

---
+++ index.da0f4209.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("#largeImg");document.querySelector("#thumbs").addEventListener("click",function(){event.preventDefault(),event.target.closest(".list-item__link");var t=event.target.closest(".list-item__link");e.src=t.href});
+//# sourceMappingURL=index.da0f4209.js.map
